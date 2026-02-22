5c9738947a75372c6ef4d5a7408581686d2cd6d4
Titutiger <99133835+Titutiger@users.noreply.github.com>
2026-02-22 21:43:56 +0530

    nothng

--- tests/test_expr.py
+++ tests/test_expr.py
@@ -1,8 +1,2 @@
 import src.ArrayGen_v2 as q
 
-X, Y, Z = q.expr('x^2 - y^2 = 1', step=0.1)
-
-import matplotlib.pyplot as plt
-plt.contour(X, Y, Z, levels=[0])
-plt.gca().set_aspect('equal')
-plt.show()
